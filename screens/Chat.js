c73199a90e4053cb6c166de2ea9db82c74b1ddbb
Bethany Polovitz <bethany.polovitz@gmail.com>
2020-05-09 11:08:47 -0500

    adding state and user information

--- screens/Chat.js
+++ screens/Chat.js
@@ -3,3 +3,32 @@ import {GiftedChat} from 'react-native-gifted=chat';
 
 import Firebase from "firebase";
 
+type Props = {
+    name?: String,
+};
+
+class Chat extends React.Component <Props>{
+    static navigationObjects = ({ navigation }) => ({
+        title: (navigation.state.params || {}).name || 'Chat!',
+    });
+
+    state = {
+        mesesages: [],
+    };
+
+    get user() {
+        return {
+            name: this.props.navigation.state.params.name,
+            _id: Firebase.shared.uid,
+        };
+    }
+
+    render() {
+        return(
+            <GiftedChat
+            messages ={this.state.messages}
+            onSend={Firebase.shared.send}
+            user={this.user} />
+        )
+    }
+}
